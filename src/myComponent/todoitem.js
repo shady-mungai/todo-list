3fe180cbd4e2a8623ebb64859ed132764233d61e
shady-mungai <shadrackmungai10@gmail.com>
2023-01-11 10:45:01 +0300

    Added a todo item

---
+++ src/myComponent/todoitem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem(){
+    return (
+        <div>
+            <h3>Learn React</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem voluptate non, deleniti totam neque aliquam labore quaerat harum reprehenderit voluptas dicta debitis voluptates officia quis inventore explicabo voluptatum minus.</p>
+        </div>
+    )
+}
+
+export default TodoItem
