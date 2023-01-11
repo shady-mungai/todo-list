7abb1b158afb967c5cada86e87006a63efa35139
shady-mungai <shadrackmungai10@gmail.com>
2023-01-11 13:00:50 +0300

    Changed h3 tag to a h2 tag

--- src/myComponent/todoitem.js
+++ src/myComponent/todoitem.js
@@ -3,7 +3,7 @@ import React from "react";
 function TodoItem(){
     return (
         <div>
-            <h3>Learn React</h3>
+            <h2> Learn React</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem voluptate non, deleniti totam neque aliquam labore quaerat harum reprehenderit voluptas dicta debitis voluptates officia quis inventore explicabo voluptatum minus.</p>
         </div>
     )
